09f0cf3a96d6cfc3b006ab7eecaefb86c838168f
Adrien Maillard <adrien.mld@laposte.net>
2019-05-02 16:32:20 +0200

    problème des ID résolu

--- Program.cs
+++ Program.cs
@@ -23,50 +23,42 @@ namespace NetduinoApplication2
 {
     public class Program
     {
+        private static String donnee;
+        private static bool compteur = false;
+        private static OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
+        private static Mfrc522 mfrc = new Mfrc522(SPI.SPI_module.SPI1, Pins.GPIO_PIN_D9, Pins.GPIO_PIN_D10);
+        private static Uid idCarte; // pour récupérer la valeur retour avec la méthode ReadUid()
         public static void Main()
         {
-            var led = new OutputPort(Pins.ONBOARD_LED, false);
-            var mfrc = new Mfrc522(SPI.SPI_module.SPI1, Pins.GPIO_PIN_D9, Pins.GPIO_PIN_D10);
-            int i = 0;
             // Request
             while (true)
             {
                 mfrc.HaltTag(); //envoie la commande halt au capteur RFID pour éviter qu'il le lise pleins de fois
-                if(mfrc.IsTagPresent()) //Verifie si il y a une carte devant le capteur
+                if (mfrc.IsTagPresent()) //Verifie si il y a une carte devant le capteur
                 {
-                    i++;
                     led.Write(true);
-                    Uid idCarte; // pour récupérer la valeur retour avec la méthode ReadUid()
-
-                    //int id = idCarte.GetHashCode();
-                    //int taille = id.ToString().Length;
-                    if (i == 1)
+                    idCarte = mfrc.ReadUid();
+                    if (idCarte.GetHashCode().ToString().Length == 9) //si l'identifiant est complet lors de la lecture
                     {
-                        idCarte = mfrc.ReadUid();
-                        mfrc.HaltTag();
-                        if (idCarte.GetHashCode().ToString().Length == 9)
-                        {
-                            mfrc.HaltTag();
-                            Debug.Print(idCarte.GetHashCode().ToString());
-                            mfrc.HaltTag();
-                        }
-                        mfrc.HaltTag();
-                        i = 0;
-                        mfrc.HaltTag();
-
+                        donnee = idCarte.GetHashCode().ToString(); //enregistre ID
+                        compteur = true;
                     }
-                    mfrc.HaltTag();
                 }
                 else
                 {
-                    mfrc.HaltTag();
                     led.Write(false);
-                    mfrc.HaltTag();
+                    Affiche(donnee); //appel de la fonction lecture quand on retire la carte
+                    compteur = false;
                 }
-                mfrc.HaltTag();
             }
-
         }
 
+        private static void Affiche(String data) //fonction qui affiche l'ID dans la console
+        {
+            if (compteur == true) //affichage des données quand "compteur" prend la valeur "true" au niveau de la détection
+            {
+                Debug.Print(data);
+            }
+        }
     }
 }
